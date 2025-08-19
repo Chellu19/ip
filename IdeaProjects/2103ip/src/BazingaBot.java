b078add8036c0ec697af3e66b3866753702f51b4
Chellu <chellu1911@gmail.com>
2025-08-19 16:54:47 +0800

    Renamed

---
+++ IdeaProjects/2103ip/src/BazingaBot.java
@@ -0,0 +1,7 @@
+public class BazingaBot {
+    public static void main(String[] args) {
+        System.out.println("Hello from BazingaBot! \n");
+        System.out.println("How may I assist you today? \n");
+        System.out.println("Live long and prosper, Bye Bye! \n");
+    }
+}
