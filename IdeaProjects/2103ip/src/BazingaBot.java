278a57935f9b7a8f0a1dc12cd8913fe2adb0f69a
Chellu <chellu1911@gmail.com>
2025-08-22 07:29:39 +0800

    Add, List

--- IdeaProjects/2103ip/src/BazingaBot.java
+++ IdeaProjects/2103ip/src/BazingaBot.java
@@ -6,18 +6,32 @@ public class BazingaBot {
         System.out.println("Hello from BazingaBot! \n");
         System.out.println("How may I assist you today? \n");
 
+        String [] arr = new String[100];
+        int counter = 0;
+
         while (true) {
             System.out.print("> ");  // Prompt
             String input = scanner.nextLine();
+
+            if(!input.equalsIgnoreCase("list") && !input.equalsIgnoreCase("bye")) {
+                if (counter < arr.length) {
+                    arr[counter] = input;
+                    counter++;
+                }
+                System.out.println("I have added to my eidetic memory: " + input);
+            }
+
+            if (input.equalsIgnoreCase("list")) {
+                for(int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + arr[i]);
+                }
+            }
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Live long and prosper, Bye Bye!");
                 break;
             }
-
-            System.out.println("You said: " + input);
         }
-
         scanner.close();
     }
 }
